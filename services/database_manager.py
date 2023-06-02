cbcb63ce8bed5f9c4526081a15d539457081e8ea
reyshazni <reyshazni@gmail.com>
2023-06-02 18:26:11 +0700

    test

--- services/database_manager.py
+++ services/database_manager.py
@@ -1,18 +1,19 @@
 from sqlalchemy import create_engine, text
-from dotenv import load_dotenv, dotenv_values
+from dotenv import load_dotenv
 
 load_dotenv()
-config = dotenv_values(".env")
 
 class DatabaseManager:
     def __init__(self, db_engine: str) -> None:
         self.conn = create_engine(db_engine).connect()
 
-db_user = config["DB_USER"]
-db_password = config["DB_PASSWORD"]
-db_host = config["DB_HOST"]
-db_port = int(config["DB_PORT"])
-db_database = config["DB_DATABASE"]
-        
-db_engine  = f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_database}"
+import os
+
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+db_database = os.getenv("DB_DATABASE")
+
+db_engine = f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_database}"
 dbInstance = DatabaseManager(db_engine=db_engine)
