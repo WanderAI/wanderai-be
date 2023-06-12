0fad6b09e227d0c7a00879d96bff421f22576c72
reyshazni <reyshazni@gmail.com>
2023-06-12 17:25:49 +0700

    feat: wiring ML services

--- routes/events.py
+++ routes/events.py
@@ -1,12 +1,13 @@
 from fastapi import APIRouter, Depends, HTTPException, Request, status, File, UploadFile
 from fastapi.responses import JSONResponse
-# from models.events import Alamat, UpdateBensin
+from models.events import ReccomendRequest
 from services.auth import AuthHandler, JWTBearer
 from services.database_manager import dbInstance
 from sqlalchemy import text
 from fastapi import FastAPI, File, UploadFile
 from PIL import Image
 import io
+import requests
 
 event_router = APIRouter(
     tags=["Events"]
@@ -50,7 +51,57 @@ async def getFormat(file: UploadFile = File(...), Authorize: JWTBearer = Depends
     except Exception as e:
         return JSONResponse(status_code=500, content={"message": str(e)})
 
+@event_router.post("/predict-image")
+async def predictImage(file: UploadFile = File(...)):
+    try:
+        # Read the file content
+        content = await file.read()
 
+        # Make sure the file is an image
+        try:
+            Image.open(io.BytesIO(content))
+        except OSError:
+            raise ValueError("Invalid file format")
+
+        # Make a POST request to be-production.com
+        response = requests.post("https://image-production-nzyzq3cmra-et.a.run.app/predict", files={"file": content})
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            # Return the response content
+            return response.json()
+        else:
+            # Return an error message if the request failed
+            error_message = response.json()
+            return JSONResponse(status_code=response.status_code, content=error_message)
+
+    except ValueError as e:
+        return JSONResponse(status_code=401, content={"message": str(e), "detail": "File format should be image format."})
+
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"message": str(e)})
+
+@event_router.post("/get-recommendation")
+async def getRecommendation(request_data: ReccomendRequest):
+    try:
+        # Convert the request data to a JSON serializable format
+        json_data = request_data.dict()
+
+        # Make a POST request to the API endpoint
+        response = requests.post("https://reccom-production-nzyzq3cmra-et.a.run.app/recommend", json=json_data)
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            # Return the response content
+            return response.json()
+        else:
+            error_message = response.json()
+            return JSONResponse(status_code=response.status_code, content=error_message)
+
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"message": str(e)})
+
+# Existing code for other endpoints
 
 # def getAverageBensin():
 #     pertamaxO = dbInstance.conn.execute(text("SELECT harga FROM bensin WHERE jenisBensin=:jenisBensin") , {"jenisBensin":"pertamax"})
